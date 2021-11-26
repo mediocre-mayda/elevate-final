4ae309fff962ea9b99fff000e3b85262bfb47732
Lubna Bargathy <loobargathy@gmail.com>
2021-11-26 21:02:05 +0200

    colors and cta section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -181,11 +181,11 @@
 
         <div class="row">
           <div class="col-lg-9 text-center text-lg-start">
-            <h3>Call To Action</h3>
-            <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h5>Support Women Business Owners, Entrepreneurs and Freelancers</h5>
+
           </div>
-          <div class="col-lg-3 cta-btn-container text-center">
-            <a class="cta-btn align-middle" href="#">Call To Action</a>
+         <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="#">Join</a>
           </div>
         </div>
 
